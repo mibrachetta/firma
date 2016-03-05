9bbd9062acee2804afb18068aaca8392ede9096f
mbrachetta <mbrachetta@gmail.com>
2016-03-05 18:51:09 -0300

    Commit from JBoss Tools

---
+++ src/isis/firma/PostSignServlet.java
@@ -0,0 +1,89 @@
+package isis.firma;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfDictionary;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfString;
+import com.itextpdf.text.pdf.security.MakeSignature.CryptoStandard;
+import com.itextpdf.text.pdf.security.PdfPKCS7;
+
+/**
+ * Servlet implementation class PostSignServlet
+ */
+@WebServlet(description = "Servlet que incrusta un firma externa en un documento PDF", urlPatterns = { "/PostSignServlet" })
+public class PostSignServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public PostSignServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			resp.setContentType("application/octet-stream");
+			System.out.println("VOY A INTENTAR INTRODUCIR LA FIRMA EN EL DOCUMENTO");
+			
+			// we get the objects we need for postsigning from the session
+			HttpSession session = req.getSession(false);
+			PdfPKCS7 sgn = (PdfPKCS7) session.getAttribute("sgn");
+			byte[] hash = (byte[]) session.getAttribute("hash");
+			PdfSignatureAppearance sap = (PdfSignatureAppearance) session.getAttribute("sap");
+			ByteArrayOutputStream os = (ByteArrayOutputStream) session.getAttribute("baos");
+			session.invalidate();
+			
+			// we read the signed bytes
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			ServletInputStream is = req.getInputStream();
+			int read;
+			byte[] data = new byte[256];
+			while ((read = is.read(data, 0, data.length)) != -1) {
+				baos.write(data, 0, read);
+			}
+			// we complete the PDF signing process
+			sgn.setExternalDigest(baos.toByteArray(), null, "RSA");
+			byte[] encodedSig = sgn.getEncodedPKCS7(hash,null,null, null, CryptoStandard.CMS);
+			byte[] paddedSig = new byte[8192];
+			System.arraycopy(encodedSig, 0, paddedSig, 0, encodedSig.length);
+			PdfDictionary dic2 = new PdfDictionary();
+			dic2.put(PdfName.CONTENTS, new PdfString(paddedSig).setHexWriting(true));
+			try {
+				sap.close(dic2);
+			} 
+			catch (DocumentException e) {
+				throw new IOException(e);
+			}
+			
+			// we write the signed document to the HttpResponse output stream
+			byte[] pdf = os.toByteArray();
+			OutputStream sos = resp.getOutputStream();
+			sos.write(pdf, 0, pdf.length);
+			sos.flush();
+			sos.close();
+		}
+}
---
+++ src/isis/firma/PreSignServlet.java
@@ -0,0 +1,129 @@
+package isis.firma;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+import java.security.MessageDigest;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.Calendar;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfDate;
+import com.itextpdf.text.pdf.PdfName;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSignature;
+import com.itextpdf.text.pdf.PdfSignatureAppearance;
+import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.security.DigestAlgorithms;
+import com.itextpdf.text.pdf.security.ExternalDigest;
+import com.itextpdf.text.pdf.security.MakeSignature.CryptoStandard;
+import com.itextpdf.text.pdf.security.PdfPKCS7;
+
+/**
+ * Servlet implementation class PreSignServlet
+ */
+@WebServlet("/PreSignServlet")
+public class PreSignServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public PreSignServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			resp.setContentType("application/octet-stream");
+			
+			try {
+				// We get the self-signed certificate from the client
+				ObjectInputStream ois = new ObjectInputStream(req.getInputStream());
+				X509Certificate cert = (X509Certificate) ois.readObject();
+				Certificate chain [] = new Certificate [1];
+				chain[0]=cert;
+								
+				//we create a reader and a stamper
+				PdfReader reader = new PdfReader(System.getenv("OPENSHIFT_DATA_DIR")+"/D0002.pdf");
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				PdfStamper stamper = PdfStamper.createSignature(reader, baos, '\0');
+				//we create the signature appearance
+				PdfSignatureAppearance sap = stamper.getSignatureAppearance();
+				sap.setReason("Prueba");
+				sap.setLocation("En servidor!");
+				sap.setVisibleSignature(new Rectangle(36, 748, 144, 780), 1, "sig");
+				sap.setCertificate(cert);
+				
+				// we create the signature infrastructure
+				PdfSignature dic = new PdfSignature(PdfName.ADOBE_PPKLITE, PdfName.ADBE_PKCS7_DETACHED);
+				dic.setReason(sap.getReason());
+				dic.setLocation(sap.getLocation());
+				dic.setContact(sap.getContact());
+				dic.setDate(new PdfDate(sap.getSignDate()));
+				sap.setCryptoDictionary(dic);
+				HashMap<PdfName, Integer> exc = new HashMap<PdfName, Integer>();
+				exc.put(PdfName.CONTENTS, new Integer(8192 * 2 + 2));
+				sap.preClose(exc);
+				
+				ExternalDigest externalDigest = new ExternalDigest() {
+					public MessageDigest getMessageDigest(String hashAlgorithm) throws GeneralSecurityException {
+						return DigestAlgorithms.getMessageDigest(hashAlgorithm, null);
+					}
+				};
+				
+				PdfPKCS7 sgn = new PdfPKCS7(null, chain, "SHA256",null, externalDigest, false);
+				InputStream data = sap.getRangeStream();
+				byte hash[] = DigestAlgorithms.digest(data,externalDigest.getMessageDigest("SHA256"));
+				byte[] sh = sgn.getAuthenticatedAttributeBytes(hash,null, null, CryptoStandard.CMS);
+
+				Calendar cal = Calendar.getInstance();
+				
+				// We store the objects we'll need for post signing in a session
+				HttpSession session = req.getSession(true);
+				session.setAttribute("sgn", sgn);
+				session.setAttribute("hash", hash);
+				session.setAttribute("cal", cal);
+				session.setAttribute("sap", sap);
+				session.setAttribute("baos", baos);
+
+				// we write the hash that needs to be signed to the HttpResponse output
+				OutputStream os = resp.getOutputStream();
+				os.write(sh, 0, sh.length);
+				os.flush();
+				os.close();
+			} 
+			catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Y NO ANDUVO");
+				e.printStackTrace();
+			}
+			catch (DocumentException e) {
+				throw new IOException(e);
+			} 
+			
+			catch (GeneralSecurityException e) {
+				throw new IOException(e);
+			}
+		}
+}
