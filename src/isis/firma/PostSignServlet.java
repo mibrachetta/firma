e0a6a9d47b0c69c7e6c2281214651a6b478be905
mbrachetta <mbrachetta@gmail.com>
2016-03-05 19:50:19 -0300

    Commit from JBoss Tools

--- src/isis/firma/PostSignServlet.java
+++ src/isis/firma/PostSignServlet.java
@@ -24,13 +24,6 @@ import com.itextpdf.text.pdf.security.PdfPKCS7;
 public class PostSignServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
