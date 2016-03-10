f5a37dd355fe80888d5d9b8573ee988e1bfcbf27
mbrachetta <mbrachetta@gmail.com>
2016-03-09 21:23:01 -0300

    Commit from JBoss Tools

--- src/isis/firma/PreSignServlet.java
+++ src/isis/firma/PreSignServlet.java
@@ -84,9 +84,9 @@ public class PreSignServlet extends HttpServlet {
 					}
 				};
 				
-				PdfPKCS7 sgn = new PdfPKCS7(null, chain, "SHA1",null, externalDigest, false);
+				PdfPKCS7 sgn = new PdfPKCS7(null, chain, "SHA256",null, externalDigest, false);
 				InputStream data = sap.getRangeStream();
-				byte hash[] = DigestAlgorithms.digest(data,externalDigest.getMessageDigest("SHA1"));
+				byte hash[] = DigestAlgorithms.digest(data,externalDigest.getMessageDigest("SHA256"));
 				System.out.println("LONGITUD HASH" + hash.length);
 				Calendar cal = Calendar.getInstance();
 				byte[] sh = sgn.getAuthenticatedAttributeBytes(hash,cal,null, null, CryptoStandard.CMS);
