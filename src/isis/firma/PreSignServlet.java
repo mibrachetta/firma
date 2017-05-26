66c92f8749a952e033ecd41bb655652f9dda169e
mbrachetta <mbrachetta@gmail.com>
2017-05-26 19:17:40 -0300

    Commit from JBoss Tools

--- src/isis/firma/PreSignServlet.java
+++ src/isis/firma/PreSignServlet.java
@@ -1,8 +1,6 @@
 package isis.firma;
 
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
