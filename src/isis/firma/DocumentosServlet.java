20b4959775512d559fbd6eb1e34ab3724f597ad0
mbrachetta <mbrachetta@gmail.com>
2017-08-22 12:51:42 -0300

    Commit from JBoss Tools

--- src/isis/firma/DocumentosServlet.java
+++ src/isis/firma/DocumentosServlet.java
@@ -25,6 +25,7 @@ public class DocumentosServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
 	}
 
 	/**
